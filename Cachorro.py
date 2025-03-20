c91121e1deb91d1b4fdc253d81257036d5a81ec7
Renan <renan.henrique3439@gmail.com>
2025-03-20 11:05:02 -0300

    Update Cachorro.py

--- Cachorro.py
+++ Cachorro.py
@@ -1,42 +1,50 @@
 class Cachorro:
-    def __init__(self, nome, raca, comida, energia):
-        self.nome = nome
-        self.raca = raca
-        self.comida = comida
-        self.acordado = False
-        self.feliz = False
+    def __init__(self, nome, raca, comida):
+        self.nome=nome
+        self.comida=comida
+        self.raca=raca
+        self.acordado=True
+        self.feliz=False
         self.energia = 80
 
     def __str__(self):
-        return f"Nome:  {self.nome}, Raça: {self.raca}, Comida: {self.comida}, Acordado: {self.acordado}, Feliz: {self.feliz}"
- 
+        return f"nome: {self.nome}, raça: {self.raca}, comida: {self.comida}, acordado: {self.acordado}, feliz: {self.feliz}"
 
-    def acordar(self):
-        self.acordado = True
-        print(f"{self.nome} acordou!")
-    
-    def dormir(self):
-        self.acordado = False
-        print(f"{self.nome} acabou dormindo!")
-    
-    def passear(self):
-        if self.feliz is True:
-           print(f"{self.nome} foi passear e ficou feliz!")
-        else:
-            print(f"{self.nome} não está feliz!")
-    
-    def ignorar(self):
-        if self.acordado is True:
-           self.feliz = False
-           print(f"Você ignorou {self.nome} e ficou triste")
+    def comer(self):
+        if self.comida>0:
+            self.comida -=1
+            self.feliz=True
+            print(f"{self.nome} comeu e agora está feliz!")
+        else: print(f"{self.nome} está sem comida!")
 
-    def brincar(self):
+    def dormir (self):
+            self.acordado = False
+            print(f"{self.nome} dormindo.")
+
+    def acordar (self):
+            self.acordado = True
+            print(f"{self.nome} está acordado!")    
+
+    def brincar (self):
         if self.acordado:
             self.energia -= 20
             self.feliz = True
-            print(f"{self.nome} está brincanco e está feliz.")
+            print(f"{self.nome} está brincando e está feliz!")
         else:
             print(f"{self.nome} não pode brincar, está dormindo...")
 
-cachorro1 = Cachorro("Kira", "Chow-Chow", 20)
-cachorro1.passear()
+    def ignorar (self):
+            self.feliz = False
+            print(f"{self.nome} está triste porque foi ignorado.")
+
+    def latir (self):
+        if self.acordado is True:
+            print(f"{self.nome} está latindo: au au au!")
+        else: print(f"{self.nome} está dormindo e não pode latir.")
+
+Cachorro1 = Cachorro("Kira", "Chow-Chow", 20)
+Cachorro1.comer()
+Cachorro1.dormir()
+Cachorro1.acordar()
+Cachorro1.brincar()
+Cachorro1.latir()
